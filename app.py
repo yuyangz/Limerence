ea189e774543245051eb00c958c002670b6edf23
Irene Lam <ilam1@stuy.edu>
2018-01-04 00:59:18 -0500

    Account authentification and devlog

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, flash, url_for
 #from util import db
+from util import db
 
 import os, cgi, hashlib
 
@@ -12,40 +13,40 @@ app.secret_key = os.urandom(32)
 global username
 
 def get_username():
-	global username
-	return username
+    global username
+    return username
 
 
 @app.route("/")
 def hello_world():
-	'''
-	If session has a record of the correct username and password input, the user is logged in
-	Otherwise, the login page is displayed
-	'''
-	if "username" in session.keys():
-		return render_template("welcome.html", name = session["username"])
-	return render_template("login.html", message = "")
+    '''
+    If session has a record of the correct username and password input, the user is logged in
+    Otherwise, the login page is displayed
+    '''
+    if "username" in session.keys():
+        return render_template("welcome.html", name = session["username"])
+    return render_template("login.html", message = "")
 
 @app.route("/createaccount")
 def create_account():
-	return render_template("createaccount.html")
+    return render_template("createaccount.html")
 
 @app.route("/auth")
 def check_creation():
-	user = request.args["username"]
-	#if request.args["username"] is unique will do later
-	if request.args["password1"] == request.args["password2"]:
-		pwd = request.args["password1"]
-		unique = db.create_account(userinfo)
-		if unique:
-			flash("Success!")
-			return redirect(url_for("hello_world"))
-		else:
-			flash ("Oops this user already exists")
-			return redirect(url_for("create_account"))
-	else:
-		flash("Passwords do not match :(")
-		return redirect(url_for("create_account"))
+    user = request.args["username"]
+    #if request.args["username"] is unique will do later
+    if request.args["password1"] == request.args["password2"]:
+        pwd = request.args["password1"]
+        unique = db.create_account(userinfo)
+        if unique:
+            flash("Success!")
+            return redirect(url_for("hello_world"))
+        else:
+            flash ("Oops this user already exists")
+            return redirect(url_for("create_account"))
+    else:
+        flash("Passwords do not match :(")
+        return redirect(url_for("create_account"))
 
 
 @app.route("/welcome")
@@ -56,24 +57,24 @@ def logged_in():
    hashed_pass = hash_object.hexdigest()
    #Validation process, what went wrong (if anything)?
    if lookup[0]:
-	  if hashed_pass == lookup[1][0]:
-		 session["username"] = input_name #Creates a new session
-		 global username
-		 username = input_name
-		 return render_template("welcome.html", name = input_name)
-	  else:
-		 return render_template("login.html", message = "Error: Wrong password")
+        if hashed_pass == lookup[1][0]:
+            session["username"] = input_name #Creates a new session
+            global username
+            username = input_name
+            return render_template("welcome.html", name = input_name)
+        else:
+            return render_template("login.html", message = "Error: Wrong password")
    else:
-	  return render_template("login.html", message =  "Error: Wrong username")
+        return render_template("login.html", message =  "Error: Wrong username")
 
 @app.route("/logout")
 def logged_out():
-	if not "username" in session.keys():
-		return redirect(url_for("hello_world"))
-	session.pop("username") #Ends session
-	return redirect("/") #Redirecting to login
+    if not "username" in session.keys():
+        return redirect(url_for("hello_world"))
+    session.pop("username") #Ends session
+    return redirect("/") #Redirecting to login
 
 if __name__ == "__main__":
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
 
--- util/db.py
+++ util/db.py
@@ -5,88 +5,88 @@ import hashlib
 path = "data/data.db"
 
 def get_db(): #returns connection to database
-	return sqlite3.connect(path)
+    return sqlite3.connect(path)
 
 def get_cursor(db): #returns cursor to database
-	return db.cursor()
+    return db.cursor()
 
 def close(db): #commits to and closes database
-	db.commit()
-	db.close()
-	
+    db.commit()
+    db.close()
+
 #============================================================#
 #============================Users=========================#
 #============================================================#
 
-'''
-	takes in dictionary of:
-		username
-		password
-		age
-		height
-		weight
-		pfplink
-		music
-		excercise
-		address
-		email
-'''
 
+'''
+    takes in dictionary of:
+        username
+        password
+        age
+        height
+        weight
+        pfplink
+        music
+        excercise
+        address
+        email
+'''
 def create_account(userinfo):
-	print "Creating Account...\n"
-	db = get_db()
-	c = get_cursor(db)
-	
-	#Add User Properties
-	command1 = "INSERT INTO accounts VALUES(?, ?, ?, ?, ?, ?, ? ,? ,?, ?)"
-	username = userinfo["username"]
-	password = userinfo["password"]
-	age = userinfo["age"]
-	height = userinfo["height"]
-	weight = userinfo["weight"]
-	pfplink = userinfo["pfplink"]
-	music = userinfo["music"]
-	excercise = userinfo["excercise"]
-	address = userinfo["address"]
-	email = userinfo["email"]
-	c.execute(command1, (username, password, age, height, weight, pfplink, music, excercise, address, email))
-	
-	#Create User Schedule Table
-	command2 = "CREATE TABLE " + username + "(time TEXT, activity TEXT, music TEXT);"
-	c.execute(command2);
-	
-	print "Created user schedule table\n"
-	close(db)
+    print "Creating Account...\n"
+    db = get_db()
+    c = get_cursor(db)
+
+    #Add User Properties
+    command1 = "INSERT INTO accounts VALUES(?, ?, ?, ?, ?, ?, ? ,? ,?, ?)"
+    username = userinfo["username"]
+    password = userinfo["password"]
+    age = userinfo["age"]
+    height = userinfo["height"]
+    weight = userinfo["weight"]
+    pfplink = userinfo["pfplink"]
+    music = userinfo["music"]
+    exercise = userinfo["excercise"]
+    address = userinfo["address"]
+    email = userinfo["email"]
+    c.execute(command1, (username, password, age, height, weight, pfplink, music, exercise, address, email))
+
+    #Create User Schedule Table
+    command2 = "CREATE TABLE " + username + "(time TEXT, activity TEXT, music TEXT);"
+    c.execute(command2)
+
+    print "Created user schedule table\n"
+    close(db)
 
 '''userinfo = {"username": "anish2000", "password":"FOURWORDSALLLOWERCASE", "age": 56, "height": 250, "weight": 100, "pfplink": "http://google.com", "music": "rock", "excercise": "cardio", "address": "345 Chambers St", "email": "email@gmail.com"}
 create_account(userinfo)'''
 
 def check_account_exist(username):
-	db = get_db()
-	c = get_cursor(db)
-	command = "SELECT username FROM accounts WHERE username = ?"
-	usernames = c.execute(command, (username,))
-	for list_username in usernames:
-		return list_username != None
-	close(db)
-	return False
+    db = get_db()
+    c = get_cursor(db)
+    command = "SELECT username FROM accounts WHERE username = ?"
+    usernames = c.execute(command, (username,))
+    for list_username in usernames:
+        return list_username != None
+    close(db)
+    return False
 
 def check_account(username, password):
-	db = get_db()
-	c = get_cursor(db)
-	if(check_account_exist(username)):
-		command = "SELECT password FROM accounts WHERE username = ? "
-		passdb = c.execute(command, (username, )).fetchone()
-		close(db)
-		return passdb[0] == password
-	close(db)
-	return False
-	
+    db = get_db()
+    c = get_cursor(db)
+    if(check_account_exist(username)):
+        command = "SELECT password FROM accounts WHERE username = ? "
+        passdb = c.execute(command, (username, )).fetchone()
+        close(db)
+        return passdb[0] == password
+    close(db)
+    return False
+
 '''
 TODO:
- 	1) Function to get entire schedule
- 	2) Function to get Activity/Music based on a give time
- 	3) Function to reset schedule daily
+    1) Function to get entire schedule
+    2) Function to get Activity/Music based on a give time
+    3) Function to reset schedule daily
 '''
 
-	
+
